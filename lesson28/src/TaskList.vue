bbcc6d31fde9f391db75452d88dad49ce4aedd12
Duilio Palacios <i@duilio.me>
2017-05-17 21:31:38 +0100

    Lesson 28

---
+++ lesson28/src/TaskList.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h2 :class="{[$style.subtitle]: hasPendingTasks }">Tareas:</h2>
+
+        <ul class="list-group tasks-list">
+            <app-task v-for="(task, index) in tasks" :key="task.id"
+                :tasks="tasks" :task="task" :index="index" @remove="deleteTask"></app-task>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    components: {
+        'app-task': Task
+    },
+    props: ['tasks'],
+    methods: {
+        deleteTask(index) {
+            this.tasks.splice(index, 1);
+        },
+    },
+    computed: {
+        hasPendingTasks() {
+            return this.tasks.some(task => task.pending);
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .tasks-list {
+        margin-bottom: 40px;
+    }
+</style>
+
+<style lang="scss" module>
+    .subtitle {
+        color: red;
+    }
+</style>
+
+
+
+
